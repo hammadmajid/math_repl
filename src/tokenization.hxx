a851727ef314f2fbfd1b81ffecad0ea31393a429
Hammad Majid <hammadmajid@proton.me>
2023-09-02 18:59:11 +0500

    move `exit_with_usage_guide()` to utils.hxx

--- src/tokenization.hxx
+++ src/tokenization.hxx
@@ -4,6 +4,8 @@
 #include <optional>
 #include <vector>
 
+#include "utils.hxx"
+
 enum class TokenType {
   int_lit,
   float_lit,
@@ -30,7 +32,7 @@ public:
     std::vector<Token> tokens;
 
     if (argc == 1) {
-      exit_with_usage_guide("");
+      utils::exit_with_usage_guide("");
     }
 
     // TODO
@@ -38,16 +40,4 @@ public:
     return tokens;
   }
 
-private:
-  void exit_with_usage_guide(std::string expr) {
-    std::cerr << "\"" << expr << "\" is not a valid expression\n\n"
-              << "Usage:\n"
-              << "\tneon [expression]\n\n"
-              << "Examples:\n"
-              << "\tneon 3 + 2^4 ==> 19\n"
-              << "\tneon 20 * ( 12 / 18) ==> 13.33333\n"
-              << "\tneon 5! ==> 120\n";
-
-    std::exit(EXIT_FAILURE);
-  }
 };
