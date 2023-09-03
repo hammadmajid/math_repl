229da84d3b52bbc6b5556ae1843bd5659884fcb1
Hammad Majid <hammadmajid@proton.me>
2023-09-03 12:51:55 +0500

    tokenize each arg

--- src/tokenization.hxx
+++ src/tokenization.hxx
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cctype>
 #include <iostream>
 #include <optional>
 #include <vector>
@@ -43,7 +44,69 @@ public:
 
     for (std::string arg : args) {
       for (int i = 0; i < arg.length(); i++) {
-        // TODO
+        if (std::isspace(arg.at(i))) {
+          continue;
+        } else if (std::isdigit(arg.at(i))) {
+          std::string buf;
+          buf.push_back(arg.at(i));
+
+          int idx = i + 1; // next value in arg
+
+          while (std::isdigit(arg[idx])) {
+            buf.push_back(arg.at(idx));
+            idx += 1;
+          }
+
+          tokens.push_back({
+              .token_type = TokenType::int_lit,
+              .value = buf,
+          });
+        } else if (arg.at(i) == '+') {
+          tokens.push_back({.token_type = TokenType::addition});
+        } else if (arg.at(i) == '-') {
+          tokens.push_back({.token_type = TokenType::substraction});
+        } else if (arg.at(i) == '*') {
+          tokens.push_back({.token_type = TokenType::multiplication});
+        } else if (arg.at(i) == '/') {
+          tokens.push_back({.token_type = TokenType::division});
+        } else if (arg.at(i) == '!') {
+          tokens.push_back({.token_type = TokenType::factorial});
+        } else if (arg.at(i) == '^') {
+          tokens.push_back({.token_type = TokenType::exponentiation});
+        } else if (arg.at(i) == '(') {
+          tokens.push_back({.token_type = TokenType::open_paren});
+        } else if (arg.at(i) == ')') {
+          tokens.push_back({.token_type = TokenType::close_paren});
+        } else {
+          utils::exit_with_usage_guide(arg);
+        }
+      }
+    }
+
+    // * for debugging
+    for (Token token : tokens) {
+      if (token.token_type == TokenType::int_lit) {
+        std::cout << "int_lit = " << token.value.value() << std::endl;
+      } else if (token.token_type == TokenType::float_lit) {
+        std::cout << "float_lit = " << token.value.value() << std::endl;
+      } else if (token.token_type == TokenType::addition) {
+        std::cout << "+" << std::endl;
+      } else if (token.token_type == TokenType::substraction) {
+        std::cout << "=" << std::endl;
+      } else if (token.token_type == TokenType::multiplication) {
+        std::cout << "*" << std::endl;
+      } else if (token.token_type == TokenType::division) {
+        std::cout << "/" << std::endl;
+      } else if (token.token_type == TokenType::exponentiation) {
+        std::cout << "^" << std::endl;
+      } else if (token.token_type == TokenType::factorial) {
+        std::cout << "!" << std::endl;
+      } else if (token.token_type == TokenType::open_paren) {
+        std::cout << "(" << std::endl;
+      } else if (token.token_type == TokenType::close_paren) {
+        std::cout << ")" << std::endl;
+      } else {
+        std::cerr << "Invalid token" << std::endl;
       }
     }
 
