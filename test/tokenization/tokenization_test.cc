63cdfa6247504033c1e10134ff45aabe43e7c020
Hammad Majid <hammadmajid@proton.me>
2023-09-07 18:26:31 +0500

    refactor: improve readability

--- test/tokenization/tokenization_test.cc
+++ test/tokenization/tokenization_test.cc
@@ -3,67 +3,25 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-TEST(test_tokenization, test_tokenize_expression) {
+TEST(TokenizationTest, TestTokenizeExpression) {
+
+  /* Arrange */
   Tokenizer tokenizer;
-
   std::string test_expr = "1 + 2.0 - 3 * 4/(5!)";
-  std::vector<Token> expected_tokens;
 
-  expected_tokens.push_back({
-      .token_type = TokenType::KIntLit,
-      .value = "1",
-  });
+  /* Define the expected tokens */
+  std::vector<Token> expected_tokens = {
+      {TokenType::KIntLit, "1"},     {TokenType::KAddition},
+      {TokenType::KFloatLit, "2.0"}, {TokenType::KSubtraction},
+      {TokenType::KIntLit, "3"},     {TokenType::KMultiplication},
+      {TokenType::KIntLit, "4"},     {TokenType::KDivision},
+      {TokenType::KOpenParen},       {TokenType::KIntLit, "5"},
+      {TokenType::KFactorial},       {TokenType::KCloseParen}};
 
-  expected_tokens.push_back({
-      .token_type = TokenType::KAddition,
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KFloatLit,
-      .value = "2.0",
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KSubtraction,
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KIntLit,
-      .value = "3",
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KMultiplication,
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KIntLit,
-      .value = "4",
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KDivision,
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KOpenParen,
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KIntLit,
-      .value = "5",
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KFactorial,
-  });
-
-  expected_tokens.push_back({
-      .token_type = TokenType::KCloseParen,
-  });
-
+  /* Act */
   std::vector<Token> actual_tokens = tokenizer.TokenizeExpression(test_expr);
 
+  /* Assert */
   ASSERT_EQ(actual_tokens.size(), expected_tokens.size());
 
   for (size_t i = 0; i < actual_tokens.size(); ++i) {
