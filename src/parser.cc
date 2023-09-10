ec18079e94e1163b38fa46b06d647043a00b4889
Hammad Majid <hammadmajid@proton.me>
2023-09-10 16:39:17 +0500

    implement peek() and comsume() method of Parser

--- src/parser.cc
+++ src/parser.cc
@@ -1,7 +1,16 @@
 #include "parser.h"
 
+std::optional<Token> Parser::peek() {
+  if (m_idx + 1 >= m_tokens.size()) {
+    return m_tokens.at(m_idx);
+  } else {
+    return {};
+  }
+}
+
+Token Parser::consume() { return m_tokens.at(m_idx++); }
+
 std::variant<AST, ParserError> Parser::ParseTokensIntoAST() {
   return ParserError{
       .err_msg = "Failed to parse tokens into AST, function no implemented yet."};
 }
-
