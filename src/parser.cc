4c52c46edd6989f039f15c5ea01b303419b61067
Hammad Majid <hammadmajid@proton.me>
2024-02-05 23:59:00 +0500

    implement the `ConvertToPostFixNotation()` method
    This is the first implementation which works for some expression that I tested it on.
    
    The method currently doesn't handle the different errors that can occur during parsing except MismatchedParentheses one.

--- src/parser.cc
+++ src/parser.cc
@@ -3,6 +3,8 @@
  * See the end file for full the license information
  */
 
+#include <stack>
+
 #include "parser.h"
 
 std::optional<Token> Parser::peek() {
@@ -30,9 +32,64 @@ unsigned short int Parser::GetOperatorPrecedence(const TokenType type)  {
 }
 
 std::variant<std::vector<Token>, ParserError> Parser::ConvertToPostFixNotation() {
-    return ParserError{
-            .type = ParserErrorType::InvalidExpression,
-            .msg = "TODO ERROR! Failed to parse and convert tokens into postfix notation.\nCause: function not implemented yet."};
+    std::vector<Token> postfix_tokens;
+    std::stack<Token> operator_stack;
+
+    while (auto maybe_token = peek()) {
+        switch (Token token = maybe_token.value(); token.token_type) {
+            case TokenType::KIntLit:
+            case TokenType::KFloatLit:
+                postfix_tokens.push_back(token);
+                consume();
+                break;
+            case TokenType::KAddition:
+            case TokenType::KDivision:
+            case TokenType::KSubtraction:
+            case TokenType::KMultiplication:
+            case TokenType::KExponentiation:
+                while (!operator_stack.empty() &&
+                       GetOperatorPrecedence(operator_stack.top().token_type) >=
+                           GetOperatorPrecedence(token.token_type)) {
+                    postfix_tokens.push_back(operator_stack.top());
+                    operator_stack.pop();
+                }
+                operator_stack.push(token);
+                consume();
+                break;
+            case TokenType::KOpenParen:
+                operator_stack.push(token);
+                consume();
+                break;
+            case TokenType::KCloseParen:
+                while (!operator_stack.empty() &&
+                       operator_stack.top().token_type != TokenType::KOpenParen) {
+                    postfix_tokens.push_back(operator_stack.top());
+                    operator_stack.pop();
+                }
+                if (operator_stack.empty() ||
+                    operator_stack.top().token_type != TokenType::KOpenParen) {
+                    // Error: mismatched parentheses
+                    return ParserError{ParserErrorType::MismatchedParentheses,
+                                       "Mismatched parentheses in expression."};
+                }
+                operator_stack.pop(); // Discard the left parenthesis
+                consume();
+                break;
+        }
+    }
+
+    // Pop any remaining operators from the stack
+    while (!operator_stack.empty()) {
+        if (operator_stack.top().token_type == TokenType::KOpenParen) {
+            // Error: unmatched left parenthesis
+            return ParserError{ParserErrorType::MismatchedParentheses,
+                               "Unmatched left parenthesis in expression."};
+        }
+        postfix_tokens.push_back(operator_stack.top());
+        operator_stack.pop();
+    }
+
+    return postfix_tokens;
 }
 
 /**
