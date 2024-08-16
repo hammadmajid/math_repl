18b9c7fbd1674f349c2394ba56396246cbdaef51
Hammad Majid <hammadmajid@proton.me>
2024-08-16 17:12:24 +0500

    Parse argumets for math expression on help flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        panic!("Expected exactly two arguments found {}", args.len());
+    }
+
+    if args[1] == "--help" {
+        println!("Usage:\n\tmath_repl \"EXPRESSION\"");
+        return;
+    }
+
+    for ch in args[1].chars() {
+        println!("{ch}");
+    }
 }
