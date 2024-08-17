b52f14e439f3243bdf4c31a0db01748710b5dbfa
Hammad Majid <hammadmajid@proton.me>
2024-08-17 22:56:05 +0500

    Parse the tokens in main after scanning them & better handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
+use std::process::exit;
 
+mod parser;
 mod scanner;
 
 #[derive(Parser)]
@@ -18,9 +20,21 @@ fn main() {
         for err in scanner.errors {
             eprintln!("{err}");
         }
-    } else {
-        for token in tokens {
-            println!("{:?}", token);
+        exit(65);
+    }
+
+    let mut parser = parser::Parser::new(tokens);
+
+    let parsed = parser.parse();
+
+    if parser.has_error {
+        for err in parser.errors {
+            eprintln!("{err}");
         }
+        exit(65);
+    }
+
+    for token in parsed {
+        println!("{:?}", token);
     }
 }
